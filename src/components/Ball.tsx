9262a3cfb7fd61c31482883c329bf9cf9dd6c648
Mathieu Joly <superpose@minimac.local>
2025-05-14 14:35:53 +0200

    add use TSL for ball texture

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,10 +1,21 @@
-import * as THREE from "three"
+import { useMemo } from "react"
+import * as THREE from "three/webgpu"
+
+import rainbow from "../textures/rainbow"
 
 function Ball({ initialPosition }: { initialPosition: THREE.Vector3 }) {
+  const material = useMemo(() => {
+    const mat = new THREE.MeshStandardNodeMaterial()
+
+    // add uTime to the shader
+    mat.colorNode = rainbow()
+
+    return mat
+  }, [])
+
   return (
-    <mesh position={initialPosition} scale={0.45}>
+    <mesh material={material} position={initialPosition} scale={0.45}>
       <icosahedronGeometry args={[1, 4]} />
-      <meshStandardMaterial color="orange" />
     </mesh>
   )
 }
